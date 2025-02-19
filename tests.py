fda8a2ce82589a14f11834b2a72f7cedcab5f777
matthewholmstrom <143293259+matthewholmstrom@users.noreply.github.com>
2025-02-19 13:04:57 -0800

    Update tests.py
    4

--- tests.py
+++ tests.py
@@ -9,5 +9,10 @@ class TestCase(unittest.TestCase):
         self.assertEqual(task.my_func(), expected)
 
 
+    def test2(self):
+        expected = "Hola World"
+        self.assertEqual(task.my_func(), expected)
+
+
 if __name__ == '__main__':
     unittest.main()
